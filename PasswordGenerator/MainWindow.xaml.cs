5c019cc0248896cca58e51f21443b8c4457fa126
thorstenmz <thorstenguenther@live.de>
2021-03-06 17:38:54 +0100

    Add project files.

---
+++ PasswordGenerator/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Animation;
+using PasswordGenerator.Properties;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private byte[] _key;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Top = Settings.Default.Top;
+            Left = Settings.Default.Left;
+            KeyBox.Focus();
+        }
+
+        private void OnGoButtonClick(object sender, RoutedEventArgs e)
+        {
+            SetKey(KeyBox.Text + " ");
+            GoButton.IsEnabled = false;
+            CopyButton.IsEnabled = true;
+            CopyButton.IsDefault = true;
+            InputBox.Visibility = Visibility.Visible;
+            ((Storyboard)Resources["FadeOutFadeIn"]).Begin();
+            InputBox.Focus();
+        }
+
+        private void OnCopyButtonClick(object sender, RoutedEventArgs e)
+        {
+            Clipboard.SetText(OutputBox.Text);
+            InputBox.SelectAll();
+            InputBox.Focus();
+        }
+
+        private void OnInputBoxKeyUp(object sender, KeyEventArgs e)
+            => SetPassword();
+
+        private void OnOptionsButtonClick(object sender, RoutedEventArgs e)
+        {
+            new OptionsWindow(this).ShowDialog();
+            SetPassword();
+        }
+
+        private void OnCloseButtonClick(object sender, RoutedEventArgs e)
+            => Close();
+
+        private void OnWindowClosing(object sender, CancelEventArgs e)
+        {
+            Settings.Default.Top = Top;
+            Settings.Default.Left = Left;
+            Settings.Default.Save();
+        }
+
+        private void SetKey(string key)
+        {
+            // Insert your code to scramble the key here.
+            _key = Encoding.Unicode.GetBytes(key);
+        }
+
+        private void SetPassword()
+            => OutputBox.Text = Settings.Default.Prefix + Cipher(InputBox.Text) + Settings.Default.Suffix;
+
+        private string Cipher(string input)
+        {
+            // Insert your code to cipher the input using the key here.
+            byte[] cipher = new byte[input.Length * 2 + _key.Length];
+            Encoding.Unicode.GetBytes(input).CopyTo(cipher, 0);
+            _key.CopyTo(cipher, input.Length * 2);
+            return Convert.ToBase64String(cipher).TrimEnd('=');
+        }
+    }
+}
---
+++ PasswordGenerator/OptionsWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using PasswordGenerator.Properties;
+
+namespace PasswordGenerator
+{
+    /// <summary>
+    /// Interaction logic for OptionsWindow.xaml
+    /// </summary>
+    public partial class OptionsWindow : Window
+    {
+        public OptionsWindow(Window owner)
+        {
+            InitializeComponent();
+            Owner = owner;
+            PrefixBox.Text = Settings.Default.Prefix;
+            SuffixBox.Text = Settings.Default.Suffix;
+            PrefixBox.Focus();
+        }
+
+        private void OnOkButtonClick(object sender, RoutedEventArgs e)
+        {
+            Settings.Default.Prefix = PrefixBox.Text;
+            Settings.Default.Suffix = SuffixBox.Text;
+            DialogResult = true;
+            Close();
+        }
+
+        private void OnCancelButtonClick(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
